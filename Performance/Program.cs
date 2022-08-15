689910ce46571bb4cf49f9c1f65c44189e1f1ccd
pedrobr <pedro.braga@targetbank.com.br>
2022-08-15 13:40:07 -0300

    feat: add performance project

---
+++ Performance/Program.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+[MinColumn, MaxColumn, BaselineColumn]
+public class ArrayVsList
+{
+    readonly int[] numbers = Enumerable.Range(0, 100).ToArray();
+    readonly List<int> numbersList = new List<int>(100);
+
+
+    [Benchmark(Baseline = true)]
+    public int GetNumberFromArray()
+    {
+        return numbers[numbers.Count() - 10];
+    }
+
+    [Benchmark]
+    public int GetNumberFromList()
+    {
+        return numbersList[numbersList.Count() - 10];
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string [] args)
+    {
+        BenchmarkRunner.Run<ArrayVsList>();
+    }
+}

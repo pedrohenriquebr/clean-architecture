b0985e7209c447519770bd813f667361729eb29c
pedrobr <pedro.braga@targetbank.com.br>
2022-08-31 00:36:38 -0300

    refactor: fixing some names

--- src/Infra.CrossCutting.Services/Repositories/BaseRepository.cs
+++ src/Infra.CrossCutting.Services/Repositories/BaseRepository.cs
@@ -7,70 +7,70 @@ public abstract class BaseRepository<T> : IBaseRepository<T>
 where T : class
 {
 
-    protected readonly IBaseRepository repository;
+    protected readonly IBaseRepository Repository;
 
-    public BaseRepository(IBaseRepository repository)
+    protected BaseRepository(IBaseRepository repository)
     {
-        this.repository = repository;
+        this.Repository = repository;
     }
 
     public Task<bool> AnyAsync()
     {
-        return repository.AnyAsync<T>();
+        return Repository.AnyAsync<T>();
     }
 
     public Task<bool> AnyAsync(Expression<Func<T, bool>> expr)
     {
-        return repository.AnyAsync(expr);
+        return Repository.AnyAsync(expr);
     }
 
     public Task<T> FirstAsync()
     {
-        return repository.FirstAsync<T>();
+        return Repository.FirstAsync<T>();
     }
 
     public Task<T> FirstAsync(Expression<Func<T, bool>> expr)
     {
-        return repository.FirstAsync(expr);
+        return Repository.FirstAsync(expr);
     }
 
     public Task<T?> FirstOrDefaultAsync()
     {
-        return repository.FirstOrDefaultAsync<T>();
+        return Repository.FirstOrDefaultAsync<T>();
     }
 
     public Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> expr)
     {
-        return repository.FirstOrDefaultAsync(expr);
+        return Repository.FirstOrDefaultAsync(expr);
     }
 
     public IQueryable<T> Query()
     {
-        return repository.Query<T>();
+        return Repository.Query<T>();
     }
 
     public IQueryable<T> Query(Expression<Func<T, bool>> expr)
     {
-        return repository.Query(expr);
+        return Repository.Query(expr);
     }
 
     public Task<T> SingleAsync()
     {
-        return repository.SingleAsync<T>();
+        return Repository.SingleAsync<T>();
     }
 
     public Task<T> SingleAsync(Expression<Func<T, bool>> expr)
     {
-        return repository.SingleAsync(expr);
+        return Repository.SingleAsync(expr);
     }
 
     public Task<T?> SingleOrDefaultAsync()
     {
-        return repository.SingleOrDefaultAsync<T>();
+        return Repository.SingleOrDefaultAsync<T>();
     }
 
     public Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> expr)
     {
-        return repository.SingleOrDefaultAsync(expr);
+        return Repository.SingleOrDefaultAsync(expr);
     }
 }
--- src/Performance/Program.cs
+++ src/Performance/Program.cs
@@ -1,23 +1,25 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
+namespace Performance;
+
 [MinColumn, MaxColumn, BaselineColumn]
 public class ArrayVsList
 {
-    readonly int[] numbers = Enumerable.Range(0, 100).ToArray();
-    readonly List<int> numbersList = new List<int>(100);
+    private readonly int[] _numbers = Enumerable.Range(0, 100).ToArray();
+    private readonly List<int> _numbersList = new(100);
 
 
     [Benchmark(Baseline = true)]
     public int GetNumberFromArray()
     {
-        return numbers[numbers.Count() - 10];
+        return _numbers[_numbers.Count() - 10];
     }
 
     [Benchmark]
     public int GetNumberFromList()
     {
-        return numbersList[numbersList.Count() - 10];
+        return _numbersList[_numbersList.Count() - 10];
     }
 }
 

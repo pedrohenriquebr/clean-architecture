08c893e6c6379ffe91a2156920a3ea12423b0d4a
pedrobr <pedro.braga@targetbank.com.br>
2022-09-06 14:01:56 -0300

    feat: depency injection extensions

---
+++ src/Infra.CrossCutting.IoC/DIExtesions.cs
@@ -0,0 +1,61 @@
+﻿using Application.Attributes;
+using Application.Common.Attributes;
+using Domain.Specifications.Common;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.CrossCutting.IoC;
+
+public static class DIExtesions
+{
+
+    public static IServiceCollection InjectServicesFrom<T>(this IServiceCollection builder)
+        where T : class
+    {
+        builder.Scan(scan =>
+                    scan.FromAssemblyOf<T>()
+                    .AddClasses(d => d.WithAttribute<ServiceAttribute>())
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
+
+        return builder;
+    }
+
+    public static IServiceCollection InjectFactoriesFrom<T>(this IServiceCollection builder)
+        where T : class
+    {
+        builder.Scan(scan =>
+                    scan.FromAssemblyOf<T>()
+                    .AddClasses(d => d.WithAttribute<FactoryAttribute>())
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
+
+        return builder;
+    }
+
+    public static IServiceCollection InjectSpecificationsFrom<T>(this IServiceCollection builder)
+        where T : class
+    {
+        builder.Scan(scan =>
+                    scan.FromAssemblyOf<T>()
+                    .AddClasses(d => d.AssignableTo(typeof(ExpressionSpecification<>)))
+                    .AsSelf()
+                    .WithScopedLifetime());
+
+        return builder;
+    }
+
+    public static IServiceCollection InjectServices(this IServiceCollection builder)
+    {
+        return builder.InjectServicesFrom<ServiceAttribute>();
+    }
+
+    public static IServiceCollection InjectFactories(this IServiceCollection builder)
+    {
+        return builder.InjectFactoriesFrom<FactoryAttribute>();
+    }
+
+    public static IServiceCollection InjectSpecifications(this IServiceCollection builder)
+    {
+        return builder.InjectSpecificationsFrom<FactoryAttribute>();
+    }
+}
---
+++ src/Infra.CrossCutting.Services.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,149 @@
+﻿using Application.Attributes;
+using Application.Common.Attributes;
+using Domain.Specifications.Common;
+using Infra.CrossCutting.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.CrossCutting.Services.Tests;
+
+public class DependencyInjectionTests
+{
+
+    public IServiceCollection Collection { get; } = new ServiceCollection();
+
+    [Fact]
+    public void Should_Inject_My_Service()
+    {
+        Collection.InjectServicesFrom<Reference>();
+
+        Assert.Equal(2, Collection.Count);
+
+        Assert.All(Collection, x =>
+        {
+            Assert.Equal(ServiceLifetime.Scoped, x.Lifetime);
+            Assert.Equal(typeof(IMyService), x.ServiceType);
+        });
+    }
+
+    [Fact]
+    public void Should_Inject_My_Factory()
+    {
+        Collection.InjectFactoriesFrom<Reference>();
+
+        Assert.Equal(1, Collection.Count);
+
+        Assert.All(Collection, x =>
+        {
+            Assert.Equal(ServiceLifetime.Scoped, x.Lifetime);
+            Assert.Equal(typeof(IProductFactory), x.ServiceType);
+        });
+    }
+
+    [Fact]
+    public void Should_Inject_My_Specifications()
+    {
+        Collection.InjectSpecificationsFrom<Reference>();
+
+        Assert.Equal(2, Collection.Count);
+
+        Assert.Contains(Collection, x => 
+        {
+            return ServiceLifetime.Scoped == x.Lifetime 
+                && typeof(ProductsWithPriceAbove10Specification) == x.ServiceType
+                && typeof(ProductsWithPriceAbove10Specification) == x.ImplementationType;
+        });
+
+        Assert.Contains(Collection, x => 
+        {
+            return ServiceLifetime.Scoped == x.Lifetime 
+                && typeof(ProductsWithPriceLower10Specification) == x.ServiceType
+                && typeof(ProductsWithPriceLower10Specification) == x.ImplementationType;
+        });
+    }
+}
+
+
+public class Reference
+{
+
+}
+
+#region Services
+public interface IMyService
+{
+    public string DoIt();
+}
+
+[Service]
+public class DefaultMyService : IMyService
+{
+    public string DoIt()
+    {
+        return nameof(DefaultMyService);
+    }
+}
+
+[Service]
+public class AnotherService : IMyService
+{
+    public string DoIt()
+    {
+        return nameof(AnotherService);
+    }
+}
+
+#endregion Services
+
+#region Factories
+public class Product
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = "";
+    public decimal Price { get; private set; }
+
+    public Product(string name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+        Price = 0;
+    }
+}
+
+public interface IProductFactory
+{
+    public Product Create(string name);    
+}
+
+
+[Factory]
+public class DefaultFactory : IProductFactory
+{
+    public Product Create(string name)
+    {
+        return new Product(name);
+    }
+}
+
+#endregion
+
+#region Especifications
+public class ProductsWithPriceAbove10Specification : ExpressionSpecification<Product>
+{
+
+    public ProductsWithPriceAbove10Specification()
+    {
+        Where(x => x.Price > 10);
+    }
+}
+
+public class ProductsWithPriceLower10Specification : ExpressionSpecification<Product>
+{
+
+    public ProductsWithPriceLower10Specification()
+    {
+        Where(x => x.Price < 10);
+    }
+}
+
+
+#endregion
